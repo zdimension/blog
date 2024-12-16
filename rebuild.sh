08610b6d4aab9c24556277014913b585dd626112
Tom Niget <zippedfire@free.fr>
2024-12-16 17:54:19 +0100

    Fix build stuff

--- rebuild.sh
+++ rebuild.sh
@@ -3,4 +3,5 @@
 whoami
 source ~/.bashrc
 source ~/.profile
+wget https://cdn.jsdelivr.net/npm/bootstrap@5/dist/css/bootstrap.min.css -O $(bundle info --path jekyll-theme-chirpy)/_sass/dist/bootstrap.css
 JEKYLL_ENV=production bundle exec jekyll build
